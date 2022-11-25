e83a80acc1b4dcf87e2a4e65e439a780ad0d1c28
Yiğit Can Pekgüzel <yigitcanpekguzel@gmail.com>
2022-11-26 02:18:27 +0300

    Add project files.

---
+++ InMemory/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            _memoryCache.Set<string>("time", DateTime.Now.ToString()); //Key,Value
+            return View();
+        }
+        public IActionResult Show()
+        {
+            ViewBag.time=_memoryCache.Get<string>("time");
+            return View(Show);
+        }
+    }
+}
