478bf07b91cec98c9730d1d45524adb68871097a
Yiğit Can Pekgüzel <yigitcanpekguzel@gmail.com>
2022-12-08 00:30:10 +0300

    BaseController,HashController CD

---
+++ StackExhange.RedisAPI.Client/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using StackExhange.RedisAPI.Web.Services;
+
+namespace StackExhange.RedisAPI.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected readonly RedisService _redisService;
+        protected readonly IDatabase _database;
+        protected readonly string listKey = "names";
+        public BaseController(RedisService redisService)
+        {
+            _redisService = redisService;
+            redisService.Connect();
+            _database = _redisService.GetDb(0);
+        }
+    }
+}
---
+++ StackExhange.RedisAPI.Client/Controllers/HashController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using StackExchange.Redis;
+using StackExhange.RedisAPI.Web.Services;
+
+namespace StackExhange.RedisAPI.Web.Controllers
+{
+    public class HashController : BaseController
+    {
+        public HashController(RedisService redisService) : base(redisService)
+        {
+            
+        }
+        public IActionResult Index()
+        {
+            Dictionary<string, string> hashlist = new Dictionary<string, string>();
+            if (_database.KeyExists(listKey))
+            {
+                _database.HashGetAll(listKey).ToList().ForEach(x =>
+                {
+                    hashlist.Add(x.Name, x.Value);
+                });
+            }
+            return View(hashlist);
+        }
+        [HttpPost]
+        public IActionResult Add(string key,string value)
+        {
+            _database.HashSet(listKey,key,value);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteItem(string key)
+        {
+            _database.HashDelete(listKey,key);
+            return RedirectToAction("Index");
+        }
+    }
+    
+    
+}
